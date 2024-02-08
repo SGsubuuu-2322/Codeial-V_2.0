52bf3d98ad7a883baae7d92ffc5ecbbba710db90
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-08 17:25:30 +0530

    Created different stylings for different body or blocks and set it up by express layouts...

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const expressLayouts = require("express-ejs-layouts");
 app.use(express.static("./assets"));
 
 app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 app.use("/", require("./routes"));
 
 app.set("view engine", "ejs");
