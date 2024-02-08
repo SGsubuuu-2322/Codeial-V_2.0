e0b8b5f8b779b2f5e9fb42aa35cb7f25c4d06037
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-08 15:47:44 +0530

    Added the code for sudivision user router....

---
+++ controllers/user_controller.js
@@ -0,0 +1,3 @@
+module.exports.profile = function (req, res) {
+  return res.end("<h1>User_Profile</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const homeController = require("../controllers/home_controller");
 
 router.get("/", homeController.home);
+router.use("/users", require("./users"));
 
 console.log("Routers Loaded...");
 module.exports = router;
