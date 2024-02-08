8e88d177e64eeee6fe8d14fcc8227138704687cd
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-08 17:11:24 +0530

    Added the static folder and styled the layout file...

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 // const path = require("path");
 
+app.use(express.static("./assets"));
+
 app.use(expressLayouts);
 app.use("/", require("./routes"));
 
