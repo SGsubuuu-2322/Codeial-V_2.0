d154bcc2f7084c85d4e3088aa5add6e23126099d
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-09 01:04:44 +0530

    Added views, controller and routes for sign-in and sign-up page...

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,14 @@ module.exports.profile = function (req, res) {
     title: "Profile",
   });
 };
+
+module.exports.userSignIn = function (req, res) {
+  return res.render("user_signin", {
+    title: "Codeial | Sign-in",
+  });
+};
+module.exports.userSignUp = function (req, res) {
+  return res.render("user_signup", {
+    title: "Codeial | Sign-up",
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require("../controllers/user_controller");
 
 router.get("/profile", userController.profile);
+router.get("/sign-in", userController.userSignIn);
+router.get("/sign-up", userController.userSignUp);
 
 module.exports = router;
