7357c34a30608aad7d9086b8d37c3c735c8f32c3
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-08 15:01:32 +0530

    Installed the express framework and started the server...

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const port = 8000;
+
+const app = express();
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`There's some error in starting your server: ${err}`);
+    return;
+  }
+
+  console.log(`Your server is successfully running on port: ${port}`);
+});
