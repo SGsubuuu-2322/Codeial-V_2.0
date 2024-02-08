9c783b8e92cacbe83e31b67dd1528d44e4fa9651
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-09 01:47:44 +0530

    installed cookie-parser and set it up...

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 // const path = require("path");
 const db = require("./configs/mongoose");
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(express.static("./assets"));
 
