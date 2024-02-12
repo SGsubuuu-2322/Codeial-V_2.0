1712609cb98ff41885d4c4ddc99b7d4a684f47a2
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-12 22:08:59 +0530

    Showcased the post lists on the home page...

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,18 @@
+const Posts = require("../models/Post");
+
 module.exports.home = function (req, res) {
   // console.log(req.cookies);
   // res.cookie("user_id", 25);
-  return res.render("home", {
-    title: "Home",
-  });
+
+  Posts.find({})
+    .then((posts) => {
+      return res.render("home", {
+        title: "Home",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      console.log("There's some issue in finding the posts from DB...");
+      return;
+    });
 };
