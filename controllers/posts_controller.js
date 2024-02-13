3b0f66b9190c185ce7be244473f0d5b2992ba91f
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-13 22:47:28 +0530

    Optimized the post controller by async await technique...

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,62 +1,35 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 
-module.exports.create = function (req, res) {
-  Post.create({
-    content: req.body.content,
-    user: req.user.id,
-  })
-    .then((post) => {
-      console.log("Successfully created the post...");
-      return res.redirect("back");
-    })
-    .catch((err) => {
-      console.log("There's some error in creating the post in the DB...", err);
-      return;
+module.exports.create = async (req, res) => {
+  try {
+    await Post.create({
+      content: req.body.content,
+      user: req.user.id,
     });
+    return res.redirect("back");
+  } catch (err) {
+    console.log("Error: ", err);
+    return;
+  }
 };
 
-module.exports.destroy = function (req, res) {
-  Post.findById(req.params.id)
-    .then((post) => {
-      if (!post) {
-        return res.redirect("/");
-      } else if (post) {
-        if (post.user == req.user.id) {
-          Post.deleteOne({ _id: req.params.id })
-            .then(() => {
-              console.log("Post deleted successfully...");
-            })
-            .catch((err) => {
-              console.log(
-                "There's some error in deleting the post from DB...",
-                err
-              );
-              return;
-            });
+module.exports.destroy = async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
 
-          Comment.deleteMany({ post: req.params.id })
-            .then(() => {
-              console.log(
-                "Comments related to the post also deleted successfully..."
-              );
-            })
-            .catch((err) => {
-              console.log(
-                "There's some issue in deleting the comments related to the post from DB...",
-                err
-              );
-              return;
-            });
+    if (!post) {
+      return res.redirect("back");
+    } else if (post) {
+      if (post.user == req.user.id) {
+        await Post.deleteOne({ _id: req.params.id });
 
-          return res.redirect("/");
-        }
-      } else {
-        return res.redirect("/");
+        await Comment.deleteMany({ post: req.params.id });
+        return res.redirect("back");
       }
-    })
-    .catch((err) => {
-      console.log("There's some issue in finding the post from DB", err);
-      return;
-    });
+    }
+  } catch (err) {
+    console.log("Error: ", err);
+    return;
+  }
 };
