05608a274d9cf82010217fcd216f45d0902dfb6b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-13 22:32:21 +0530

    Optimized the comment controller codes through async await...

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,77 +1,47 @@
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 
-module.exports.create = function (req, res) {
-  Post.findById(req.body.post)
-    .then((post) => {
-      if (post) {
-        Comment.create({
-          content: req.body.content,
-          post: req.body.post,
-          user: req.user.id,
-        })
-          .then((comment) => {
-            post.comments.push(comment);
-            post.save();
-            return res.redirect("back");
-          })
-          .catch((err) => {
-            console.log(
-              "There's some issue in creating a comment in the DB",
-              err
-            );
-            return;
-          });
-      }
-    })
-    .catch((err) => {
-      console.log(
-        "Error in finding the user from DB for saving comment onto it...",
-        err
-      );
-      return;
+module.exports.create = async (req, res) => {
+  try {
+    let post = await Post.findById(req.body.post);
+
+    let comment = await Comment.create({
+      content: req.body.content,
+      post: req.body.post,
+      user: req.user.id,
     });
+
+    post.comments.push(comment);
+    post.save();
+    return res.redirect("back");
+  } catch (err) {
+    console.log("Error: ", err);
+    return;
+  }
 };
 
-module.exports.destroy = function (req, res) {
-  Comment.findById(req.params.id)
-    .then((comment) => {
-      if (!comment) {
+module.exports.destroy = async (req, res) => {
+  try {
+    let comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      return res.redirect("back");
+    } else if (comment) {
+      if (req.user.id == comment.user) {
+        const postID = comment.post;
+
+        let cmt = await Comment.deleteOne({ _id: req.params.id });
+
+        let post = await Post.findByIdAndUpdate(postID, {
+          $pull: { comments: req.params.id },
+        });
         return res.redirect("back");
-      } else if (comment) {
-        if (req.user.id == comment.user) {
-          const postID = comment.post;
-
-          Comment.deleteOne({ _id: req.params.id })
-            .then(() => {
-              console.log("Comment deleted successfully...");
-            })
-            .catch((err) => {
-              console.log("Error in deleting the comment from DB...", err);
-              return;
-            });
-
-          Post.findByIdAndUpdate(postID, {
-            $pull: { comments: req.params.id },
-          })
-            .then((post) => {
-              console.log(
-                "Updated the post after removing the comment from comments array..."
-              );
-              return res.redirect("back");
-            })
-            .catch((err) => {
-              console.log(
-                "Error in finding post and updating its comments array...",
-                err
-              );
-              return;
-            });
-        }
+      } else {
+        return res.redirect("back");
       }
-    })
-    .catch((err) => {
-      console.log("Error in finding the comment from DB...".err);
-      return;
-    });
+    }
+  } catch (err) {
+    console.log("Error: ", err);
+    return;
+  }
 };
