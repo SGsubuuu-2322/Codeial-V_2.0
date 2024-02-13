5fecf58a1fe4c623477fd74cd5129e777792f514
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-13 22:39:31 +0530

    Debugged some comment controller codes...

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -5,15 +5,17 @@ module.exports.create = async (req, res) => {
   try {
     let post = await Post.findById(req.body.post);
 
-    let comment = await Comment.create({
-      content: req.body.content,
-      post: req.body.post,
-      user: req.user.id,
-    });
+    if (post) {
+      let comment = await Comment.create({
+        content: req.body.content,
+        post: req.body.post,
+        user: req.user.id,
+      });
 
-    post.comments.push(comment);
-    post.save();
-    return res.redirect("back");
+      post.comments.push(comment);
+      post.save();
+      return res.redirect("back");
+    }
   } catch (err) {
     console.log("Error: ", err);
     return;
