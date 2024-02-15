0089c24108e4ddf26dd10899c8430047f961c7b5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-16 00:36:48 +0530

    Added feature to submit the form data through ajax technique on views side and then handled it on server side for creating post and giving response as JSON...

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,3 +1,22 @@
 {
-  console.log("Hiii there....");
+  let createPostForm = function () {
+    let newPostForm = $("#new-post-form");
+
+    newPostForm.submit((e) => {
+      e.preventDefault();
+      $.ajax({
+        type: "post",
+        url: "/posts/create",
+        data: newPostForm.serialize(),
+        success: function (data) {
+          console.log(data);
+        },
+        error: function (err) {
+          console.log(err.responseText);
+        },
+      });
+    });
+  };
+
+  createPostForm();
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,20 @@ const Comment = require("../models/comment");
 
 module.exports.create = async (req, res) => {
   try {
-    await Post.create({
+    let post = await Post.create({
       content: req.body.content,
       user: req.user.id,
     });
+
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "Post Created!!!",
+      });
+    }
+
     req.flash("success", "Post Created Successfully...");
     return res.redirect("back");
   } catch (err) {
