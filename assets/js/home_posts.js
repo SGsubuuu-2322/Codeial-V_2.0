081d68bc25ce940735a263e1bdf688d8d9c55529
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-16 12:44:56 +0530

    Added the scripts for adding the received post onto DOM...

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,12 +10,49 @@
         data: newPostForm.serialize(),
         success: function (data) {
           console.log(data);
+          let post = showPostOnDom(data.data.post);
+          $(".post-lists").prepend(post);
         },
         error: function (err) {
           console.log(err.responseText);
         },
       });
     });
+  };
+
+  let showPostOnDom = function (post) {
+    return $(` 
+    <li id="post-${post._id}">
+    <p>
+      <small><a class="delete-post-button" href="/posts/destroy/${post.id}">X</a></small>
+     ${post.content}
+      <br />
+      <small> - ${post.user.name}</small>
+    </p>
+  
+    <div class="post-comments">
+
+      <form action="/comments/create" method="post" class="new-comment-form">
+        <input
+          type="text"
+          name="content"
+          placeholder="Type here to add comment..."
+          required
+        />
+        <input type="hidden" name="post" value="${post._id}" />
+        <button type="submit">Add Comment</button>
+      </form>
+
+  
+      <div class="post-comments-list">
+        <ul id="post-comment-${post._id}">
+         
+        </ul>
+      </div>
+    </div>
+  </li>
+  
+    `);
   };
 
   createPostForm();
