28cf0ec4dd3af044e192e37339329dd2c6b9627c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-20 00:02:52 +0530

    Added the code v2 post api for post deletion for trial purpose...

--- controllers/api/v2/posts_api.js
+++ controllers/api/v2/posts_api.js
@@ -1,6 +1,58 @@
-module.exports.index = function (req, res) {
+const Posts = require("../../../models/post");
+const Comments = require("../../../models/comment");
+
+module.exports.index = async function (req, res) {
+  let posts = await Posts.find({})
+    .sort("createdAt")
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+
   return res.json(200, {
     message: "Lists of posts for v2_api",
-    posts: [],
+    posts: posts,
   });
+};
+
+module.exports.destroy = async (req, res) => {
+  try {
+    let post = await Posts.findById(req.params.id);
+
+    if (!post) {
+      return res.redirect("back");
+    } else if (post) {
+      // if (post.user == req.user.id) {
+      await Posts.deleteOne({ _id: req.params.id });
+
+      await Comments.deleteMany({ post: req.params.id });
+
+      //   if (req.xhr) {
+      //     return res.status(200).json({
+      //       data: {
+      //         post_id: req.params.id,
+      //       },
+      //       message: "Post and its associated comments deleted successfully...",
+      //     });
+      //   }
+
+      //   req.flash(
+      //     "success",
+      //     "Post and its associated comments deleted successfully..."
+      //   );
+      return res.json(200, {
+        message: "Post deleted successfully...",
+        posts_id: req.params.id,
+      });
+    }
+    //   }
+  } catch (err) {
+    //   req.flash("error", err);
+    return res.json(500, {
+      message: "Internal server damage...",
+    });
+  }
 };
--- routes/api/v2/posts.js
+++ routes/api/v2/posts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postApi = require("../../../controllers/api/v2/posts_api");
 
 router.get("/", postApi.index);
+router.delete("/:id", postApi.destroy);
 
 module.exports = router;
