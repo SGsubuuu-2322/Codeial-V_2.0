3ae73d1635fa28ed9c3ddbbe053d3f9bc9d43a4b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-21 02:49:29 +0530

    Installed and setup the nodemailer library for sending mails...

---
+++ configs/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "pradhansubham2322",
+    password: "SGshreyans(@232214)",
+  },
+});
+
+let renderTemlate = (data, relativePath) => {
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("Error in creating mail template...", err);
+        return;
+      }
+
+      mailHTML = template;
+    }
+  );
+
+  return mailHTML;
+};
+
+module.exports = {
+  transporter: transporter,
+  renderTemlate: renderTemlate,
+};
