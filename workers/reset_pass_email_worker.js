613aca36cdb7e7a300eb98280ab8f15417bd856c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-22 22:59:47 +0530

    Added reset_password_email worker for sending the reset password link to the requested user email with the unique acess token in background....

--- workers/reset_pass_email_worker.js
+++ workers/reset_pass_email_worker.js
@@ -1,9 +1,9 @@
-// const queue = require("../configs/kue");
+const queue = require("../configs/kue");
 
-// const resetPasswordMailer = require("../mailers/reset_password_mailer");
+const resetPasswordMailer = require("../mailers/reset_password_mailer");
 
-// queue.process("sendResetPasswordEmail", (job, done) => {
-//   resetPasswordMailer.newResetPassEmail(job.data);
+queue.process("sendResetPasswordEmail", (job, done) => {
+  resetPasswordMailer.newResetPassEmail(job.data);
 
-//   done();
-// });
+  done();
+});
