18d0c81651c3b95df078500b4f3f7e4ac97a790b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-22 23:20:31 +0530

    Added success reset password mailer for sending the updation email to the updated user email...

--- mailers/success_pass_reset_mailer.js
+++ mailers/success_pass_reset_mailer.js
@@ -1,33 +1,33 @@
-// const nodeMailer = require("../configs/nodemailer");
+const nodeMailer = require("../configs/nodemailer");
 
-// exports.newSuccessPassEmail = (successPassToken) => {
-//   let htmlString = nodeMailer.renderTemlate(
-//     {
-//       successPassToken: successPassToken,
-//     },
-//     "/success_reset_pass/success_reset_pass_email.ejs"
-//   );
+exports.newSuccessPassEmail = (successPassToken) => {
+  let htmlString = nodeMailer.renderTemlate(
+    {
+      successPassToken: successPassToken,
+    },
+    "/success_reset_pass/success_reset_pass_email.ejs"
+  );
 
-//   nodeMailer.transporter.sendMail(
-//     {
-//       from: {
-//         name: "Dev_Subuuu",
-//         address: "pradhansubham2322@gmail.com",
-//       },
-//       to: [successPassToken.user.email],
-//       subject: "Successfull Password Reset!!!",
-//       html: htmlString,
-//     },
-//     (err, info) => {
-//       if (err) {
-//         console.log(
-//           "Error in sending the email for successfull Password Reset...",
-//           err
-//         );
-//         return;
-//       }
-//       console.log("Email_Info: ", info);
-//       return;
-//     }
-//   );
-// };
+  nodeMailer.transporter.sendMail(
+    {
+      from: {
+        name: "Dev_Subuuu",
+        address: "pradhansubham2322@gmail.com",
+      },
+      to: [successPassToken.user.email],
+      subject: "Successfull Password Reset!!!",
+      html: htmlString,
+    },
+    (err, info) => {
+      if (err) {
+        console.log(
+          "Error in sending the email for successfull Password Reset...",
+          err
+        );
+        return;
+      }
+      console.log("Email_Info: ", info);
+      return;
+    }
+  );
+};
