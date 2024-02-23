fbf132c99a9b57043bc6378f46e1938bbe25ca8b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-23 16:23:55 +0530

    Created the like model for storing all like on our web-app...

---
+++ models/like.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const likeSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+
+    likeable: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      refPath: "onModel",
+    },
+
+    onModel: {
+      type: String,
+      require: true,
+      enum: ["Post", "Comment"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Like = mongoose.model("Like", likeSchema);
+module.exports = Like;
