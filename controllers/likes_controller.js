a4d9e4406f8366d9623e6e033b54d725efe48acf
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-23 17:33:06 +0530

    Created the togglelike controller and its action for toggling the like...

---
+++ controllers/likes_controller.js
@@ -0,0 +1,37 @@
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+const Like = require("../models/like");
+
+module.exports.toggleLike = async function (req, res) {
+  let likeable;
+  let deleted = false;
+
+  if (req.query.type == "Post") {
+    likeable = await Post.findById(req.query.id).populate("likes");
+  } else {
+    likeable = await Comment.findById(req.query.id).populate("likes");
+  }
+
+  let existingLike = await Like.findOne({
+    user: req.user._id,
+    likeable: req.query.id,
+    onModel: req.query.type,
+  });
+
+  if (existingLike) {
+    likeable.likes.pull(existingLike._id);
+    likeable.save();
+
+    existingLike.remove();
+    deleted = true;
+  } else {
+    let newLike = await Like.create({
+      user: req.user._id,
+      likeable: req.query.id,
+      onModel: req.query.type,
+    });
+
+    likeable.likes.push(newLike.id);
+    likeable.save();
+  }
+};
