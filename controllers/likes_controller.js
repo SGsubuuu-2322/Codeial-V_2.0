f750368cc21ca5472535a000bd29f50a84e413bd
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-24 21:14:25 +0530

    Added codes for likes controller for modifying likes status in db and as per post and comment and send response according to that...

--- controllers/likes_controller.js
+++ controllers/likes_controller.js
@@ -1,63 +1,63 @@
-// const Post = require("../models/post");
-// const Comment = require("../models/comment");
-// const Like = require("../models/like");
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+const Like = require("../models/like");
 
-// module.exports.toggleLike = async function (req, res) {
-//   try {
-//     let likeable;
-//     let deleted = false;
+module.exports.toggleLike = async function (req, res) {
+  try {
+    let likeable;
+    let deleted = false;
 
-//     // console.log("Id: ", req.query.id);
-//     // console.log("Type: ", req.query.type);
+    // console.log("Id: ", req.query.id);
+    // console.log("Type: ", req.query.type);
 
-//     if (req.query.type == "Post") {
-//       likeable = await Post.findById(req.query.id).populate("likes");
-//     } else {
-//       likeable = await Comment.findById(req.query.id).populate("likes");
-//     }
+    if (req.query.type == "Post") {
+      likeable = await Post.findById(req.query.id).populate("likes");
+    } else {
+      likeable = await Comment.findById(req.query.id).populate("likes");
+    }
 
-//     // console.log("Likeable: ", likeable);
+    // console.log("Likeable: ", likeable);
 
-//     let existingLike = await Like.findOne({
-//       user: req.user._id,
-//       likeable: req.query.id,
-//       onModel: req.query.type,
-//     });
+    let existingLike = await Like.findOne({
+      user: req.user._id,
+      likeable: req.query.id,
+      onModel: req.query.type,
+    });
 
-//     if (existingLike) {
-//       // console.log("Existing Like id: ", existingLike._id);
-//       likeable.likes.pull(existingLike._id);
-//       likeable.save();
+    if (existingLike) {
+      // console.log("Existing Like id: ", existingLike._id);
+      likeable.likes.pull(existingLike._id);
+      likeable.save();
 
-//       await Like.findByIdAndDelete(existingLike._id);
-//       deleted = true;
+      await Like.findByIdAndDelete(existingLike._id);
+      deleted = true;
 
-//       // console.log("Likeable likes: ", likeable.likes);
-//     } else {
-//       let newLike = await Like.create({
-//         user: req.user._id,
-//         likeable: req.query.id,
-//         onModel: req.query.type,
-//       });
+      // console.log("Likeable likes: ", likeable.likes);
+    } else {
+      let newLike = await Like.create({
+        user: req.user._id,
+        likeable: req.query.id,
+        onModel: req.query.type,
+      });
 
-//       // console.log("newLike: ", newLike);
-//       // console.log("Likeable Like: ", likeable.likes);
+      // console.log("newLike: ", newLike);
+      // console.log("Likeable Like: ", likeable.likes);
 
-//       likeable.likes.push(newLike._id);
-//       likeable.save();
-//       // console.log("Likeable Like: ", likeable.likes);
-//     }
+      likeable.likes.push(newLike._id);
+      likeable.save();
+      // console.log("Likeable Like: ", likeable.likes);
+    }
 
-//     return res.status(200).json({
-//       message: "Request Successfull...",
-//       data: {
-//         deleted: deleted,
-//       },
-//     });
-//   } catch (err) {
-//     if (("Error in manipulating the likes...", err));
-//     return res.status(500).json({
-//       message: "Internal server error...",
-//     });
-//   }
-// };
+    return res.status(200).json({
+      message: "Request Successfull...",
+      data: {
+        deleted: deleted,
+      },
+    });
+  } catch (err) {
+    if (("Error in manipulating the likes...", err));
+    return res.status(500).json({
+      message: "Internal server error...",
+    });
+  }
+};
