7e37e5ddcfc0199fcbe0c1fd02c82c0ed0f54fdf
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-24 21:29:06 +0530

    Added js for toggling the button through Jquery AJAX call and manipulate it asynchronously...

--- assets/js/toggle_likes.js
+++ assets/js/toggle_likes.js
@@ -1,34 +1,34 @@
-// class ToggleLike {
-//   constructor(toggleElement) {
-//     this.toggler = toggleElement;
-//     this.toggleLike();
-//   }
+class ToggleLike {
+  constructor(toggleElement) {
+    this.toggler = toggleElement;
+    this.toggleLike();
+  }
 
-//   toggleLike() {
-//     $(this.toggler).click(function (e) {
-//       e.preventDefault();
-//       let self = this;
+  toggleLike() {
+    $(this.toggler).click(function (e) {
+      e.preventDefault();
+      let self = this;
 
-//       $.ajax({
-//         type: "Post",
-//         url: $(self).attr("href"),
-//       })
-//         .done(function (data) {
-//           let likesCount = parseInt($(self).attr("data-likes"));
-//           if (data.data.deleted == true) {
-//             likesCount--;
-//           } else {
-//             likesCount++;
-//           }
-//           console.log("Likes Count is: ", likesCount);
+      $.ajax({
+        type: "Post",
+        url: $(self).attr("href"),
+      })
+        .done(function (data) {
+          let likesCount = parseInt($(self).attr("data-likes"));
+          if (data.data.deleted == true) {
+            likesCount--;
+          } else {
+            likesCount++;
+          }
+          console.log("Likes Count is: ", likesCount);
 
-//           $(self).attr("data-likes", likesCount);
-//           $(self).html(`${likesCount}Likes`);
-//         })
-//         .fail((err) => {
-//           console.log("Error in completing the request...", err);
-//           return;
-//         });
-//     });
-//   }
-// }
+          $(self).attr("data-likes", likesCount);
+          $(self).html(`${likesCount}Likes`);
+        })
+        .fail((err) => {
+          console.log("Error in completing the request...", err);
+          return;
+        });
+    });
+  }
+}
