315534775d58bf6d15590a3df5e918f5537e3ce6
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-25 23:13:55 +0530

    Created the friend_profile js file to interact with backend server asynchronously for toggling friendship status...

--- assets/js/friend_profile.js
+++ assets/js/friend_profile.js
@@ -1,24 +1,24 @@
-// (function () {
-//   const toggleFriendship = $(".toggle-friendship");
-//   const link = $("#link");
+(function () {
+  const toggleFriendship = $(".toggle-friendship");
+  const link = $("#link");
 
-//   toggleFriendship.click((e) => {
-//     e.preventDefault();
+  toggleFriendship.click((e) => {
+    e.preventDefault();
 
-//     $.ajax({
-//       type: "get",
-//       url: link.attr("href"),
-//     }).done(function (data) {
-//       let friendShip = data.data.isFriend;
-//       // console.log(friendShip);
+    $.ajax({
+      type: "get",
+      url: link.attr("href"),
+    }).done(function (data) {
+      let friendShip = data.data.isFriend;
+      // console.log(friendShip);
 
-//       let friendBox = $(".friend-box");
+      let friendBox = $(".friend-box");
 
-//       if (friendShip) {
-//         friendBox.html(`<span>Friend</span>`);
-//       } else {
-//         friendBox.html("");
-//       }
-//     });
-//   });
-// })();
+      if (friendShip) {
+        friendBox.html(`<span>Friend</span>`);
+      } else {
+        friendBox.html("");
+      }
+    });
+  });
+})();
