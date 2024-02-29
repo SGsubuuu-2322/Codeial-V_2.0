8cebe49217b28490c26fc0669783b57274eebe36
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-29 21:19:48 +0530

    Added Some meaningful comments for our app's entry-point index.js file...

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
+// Importing express framework for creating server and its logical operations smoothly on top of Node JS. ....
 const express = require("express");
+// Importing environment file for accessing environment variables...
 const env = require("./configs/environment");
+// Importing  cookie-parser to parse payloads from request cookies  into JSON object...
 const cookieParser = require("cookie-parser");
+// Creating an instance of the express app, and defining  a port number to run on.
 const app = express();
 const port = 8000;
+
+// Importing express-ejs-layouts for creating layout for our web-app...
 const expressLayouts = require("express-ejs-layouts");
+// Importing path  module to resolve paths for varoius files from various folders...
 const path = require("path");
+
+// Importing the mongoDB connection in our entry-point file...
 const db = require("./configs/mongoose");
 
+// Importing express-session for handling and creating sesssions and its data...
 const session = require("express-session");
+
+// Importing passport library and its configured strategies for handling authentication and authorisation...
 const passport = require("passport");
 const passportLocal = require("./configs/passport_local_strategy");
 const passportJWT = require("./configs/passport-jwt-strategy");
 const passportGoogleStrategy = require("./configs/passport-google-oauth-strategy");
+
+// Importing connect-mongo as  the MongoDB store for Express Session middleware for storing session data...
 const MongoStore = require("connect-mongo");
+
+// Importing node-sass-middleware  for compiling sass to css at runtime...
 const sassMiddleware = require("node-sass-middleware");
+
+// Importing connect-flash and configured customWare for  flash messages functionality...
 const flash = require("connect-flash");
 const customMware = require("./configs/middleware");
 
+// Importing http module and  using it to create HTTP server that listens on specified port for our chatserver...
 const chatServer = require("http").Server(app);
 const chatSockets = require("./configs/chat_sockets").chatSocket(chatServer);
 chatServer.listen(5000);
 console.log("Chating Server is listening on port: 5000");
 
+
+// Creating custom middleware for setting up node-sass-middleware as for using scss technology  in stylesheets..
 app.use(
   sassMiddleware({
     src: path.join(__dirname, env.assets_path, "/scss"),
